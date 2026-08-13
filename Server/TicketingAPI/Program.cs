956f63e4982619c8cf8cbb7c68cb646e063b4374
SYLCN05 <Siyaryalcin25@gmail.com>
2026-08-13 14:33:57 +0200

    Setup a basic DB connection with postgresql

--- Server/TicketingAPI/Program.cs
+++ Server/TicketingAPI/Program.cs
@@ -1,3 +1,5 @@
+using Npgsql;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,9 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+await using var conn = new NpgsqlConnection(builder.Configuration.GetConnectionString("DefaultConnection"));
+await conn.OpenAsync();
+Console.WriteLine($"The PostgreSQL version: {conn.PostgreSqlVersion}"); 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
