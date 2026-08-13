6af69f6226995a578018e8f01262198e325545b8
SYLCN05 <Siyaryalcin25@gmail.com>
2026-08-13 16:02:30 +0200

    Add tickets model, DB context and create endpoint

---
+++ Server/TicketingAPI/Data/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TicketingAPI.Models;
+
+namespace TicketingAPI.Data
+{
+    public class ApplicationDBContext: DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) :base(options)
+        {
+        }
+
+        public DbSet<tickets> tickets { get; set; }
+    }
+}
