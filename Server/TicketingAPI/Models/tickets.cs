5d27391c799aae8427a42bc01df199c25f17a764
SYLCN05 <Siyaryalcin25@gmail.com>
2026-08-13 22:27:00 +0200

    Switch ticket date to DateTime; update PUT route

--- Server/TicketingAPI/Models/tickets.cs
+++ Server/TicketingAPI/Models/tickets.cs
@@ -5,6 +5,6 @@
         public int id { get; set; }
         public string ticket_title { get; set; }
         public string ticket_description { get; set; }
-        public DateOnly ticket_turnin_date { get; set; } = new DateOnly();
+        public DateTime ticket_turnin_date { get; set; } = DateTime.UtcNow;  
     }
 }
