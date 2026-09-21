2f8cb66073faf72edc533a2f07af3295a43240fd
SYLCN05 <Siyaryalcin25@gmail.com>
2026-09-21 22:43:43 +0200

    Add MUI icons and update styles in Header and Tickets components

--- Client/ticketing-frontend/src/Home/Home.tsx
+++ Client/ticketing-frontend/src/Home/Home.tsx
@@ -1,11 +1,9 @@
-import { Route, Routes } from "react-router-dom";
-import { Tickets } from "./Tickets";
 export function Home() {
   return (
     <div>
-      <div className="flex items-center justify-center bg-[url(/Backgroundhome.jpg)] w-full h-140 bg-cover brightness-100">
+      <div className="flex items-center justify-center bg-#DBE2EF w-full h-140 bg-cover brightness-100">
         <div className=" flex flex-col text-lg text-white font-mono w-2xl h-50 justify-between text-center">
-          <h2 className="text-6xl">Ticketing System</h2>
+          <h2 className="text-6xl font-mono">Ticketing System</h2>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
             voluptatem quia enim eum quidem quisquam ea est officia nostrum
--- Client/ticketing-frontend/src/Home/Tickets.tsx
+++ Client/ticketing-frontend/src/Home/Tickets.tsx
@@ -31,16 +31,23 @@ export function Tickets() {
     return <h1>[error]</h1>;
   }
   return (
-    <div>
-      <h2>Tickets:</h2>
-
-      <ul>
+    <div className="">
+      <ul className="flex flex-wrap gap-4 items-center justify-center mt-20 text-theme-light font-mono  ">
         {!loading &&
           tickets.map((ticket) => (
-            <li key={ticket.id}>
-              <h2>{ticket.ticket_title}</h2>
-              <p>{ticket.ticket_description}</p>
+            <li
+              key={ticket.id}
+              className="bg-theme-blue w-md border-r-3 rounded-md p-4 max-w-2xl min-h-40 max-h-40 overflow-y-auto "
+            >
               <h3>{ticket.ticket_priority}</h3>
+              <h2 className="text-center text-lg">{ticket.ticket_title}</h2>
+              <p>{ticket.ticket_description}</p>
+              <button
+                className="bg-theme-navy hover:bg-theme-blue rounded-xl min-w-20 min-h-10 mt-2"
+                onClick={() => console.log("heloooo")}
+              >
+                Details
+              </button>
             </li>
           ))}
       </ul>
